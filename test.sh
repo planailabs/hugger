fac41d88af6fbd2d07e7b3fae1fac1a3fa08001d
Maciej Krüger <mkg20001@gmail.com>
2026-07-05 20:35:13 +0200

    ci: move tests to test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Unit + HTTP integration + live-Hub tests (live tests skip when offline).
+nix develop .#test -c python tests/test_core.py
+nix develop .#test -c python tests/test_api.py
+nix develop .#test -c python tests/test_hub_live.py
